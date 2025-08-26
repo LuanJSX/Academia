399352241b427c54dd357dae9a64e8de79baa93a
Luan Batista <69101674+LuanJSX@users.noreply.github.com>
2025-08-26 13:46:42 -0300

    up2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./assets/components/Navbar.jsx";
 import { Banner } from "./assets/components/Banner.jsx";
 import { Wcu } from "./assets/components/Wcu.jsx";
-import { Mt } from "./assets/components/mt.jsx";
+import { Mt } from "./assets/components/Mt.jsx";
 import { Princing } from "./assets/components/Princing.jsx";
 import { YourOpinions } from "./assets/components/YourOpinions.jsx";
 import { Contact } from "./assets/components/Contact.jsx";
